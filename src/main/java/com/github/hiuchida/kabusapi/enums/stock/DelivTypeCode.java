8a48142b98560b8e03c7d8dbe38e8191a13cc105
hiuchida <hiro4uchida@gmail.com>
2023-07-08 20:11:34 +0900

    Add DelivTypeCode

---
+++ src/main/java/com/github/hiuchida/kabusapi/enums/stock/DelivTypeCode.java
@@ -0,0 +1,36 @@
+package com.github.hiuchida.kabusapi.enums.stock;
+
+/**
+ * 受渡区分。
+ */
+public enum DelivTypeCode {
+	指定なし(0), 自動振替(1), お預り金(2);
+
+	private int id;
+
+	private DelivTypeCode(int id) {
+		this.id = id;
+	}
+
+	public int intValue() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return Integer.toString(id);
+	}
+
+	public static DelivTypeCode valueOf(Integer id) {
+		if (id == null) {
+			return null;
+		}
+		for (DelivTypeCode e : values()) {
+			if (e.id == id) {
+				return e;
+			}
+		}
+		throw new IllegalArgumentException("No enum constant id=" + id);
+	}
+
+}
---
+++ src/test/java/com/github/hiuchida/kabusapi/enums/stock/DelivTypeCodeTest.java
@@ -0,0 +1,31 @@
+package com.github.hiuchida.kabusapi.enums.stock;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DelivTypeCodeTest {
+
+	@Test
+	public void intValueTest() {
+		assertEquals(0, DelivTypeCode.指定なし.intValue());
+		assertEquals(1, DelivTypeCode.自動振替.intValue());
+		assertEquals(2, DelivTypeCode.お預り金.intValue());
+	}
+
+	@Test
+	public void toStringTest() {
+		assertEquals("0", DelivTypeCode.指定なし.toString());
+		assertEquals("1", DelivTypeCode.自動振替.toString());
+		assertEquals("2", DelivTypeCode.お預り金.toString());
+	}
+
+	@Test
+	public void valueOfTest() {
+		assertEquals(null, DelivTypeCode.valueOf((Integer) null));
+		assertEquals(DelivTypeCode.指定なし, DelivTypeCode.valueOf(0));
+		assertEquals(DelivTypeCode.自動振替, DelivTypeCode.valueOf(1));
+		assertEquals(DelivTypeCode.お預り金, DelivTypeCode.valueOf(2));
+	}
+
+}
