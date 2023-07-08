b2f5f70595981297f770fb01229710938dea5dc0
hiuchida <hiro4uchida@gmail.com>
2023-07-08 23:14:42 +0900

    Add FundTypeCode

---
+++ src/main/java/com/github/hiuchida/kabusapi/enums/stock/FundTypeCode.java
@@ -0,0 +1,29 @@
+package com.github.hiuchida.kabusapi.enums.stock;
+
+/**
+ * 資産区分（預り区分）。
+ */
+public enum FundTypeCode {
+	現物売("  "), 保護("02"), 信用代用("AA"), 信用取引("11");
+
+	private String code;
+
+	private FundTypeCode(String code) {
+		this.code = code;
+	}
+
+	@Override
+	public String toString() {
+		return code;
+	}
+
+	public static FundTypeCode valueOfCode(String code) {
+		for (FundTypeCode e : values()) {
+			if (e.code.equals(code)) {
+				return e;
+			}
+		}
+		throw new IllegalArgumentException("No enum constant code=" + code);
+	}
+
+}
